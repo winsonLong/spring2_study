f1ff7ee449aa4d2d75a1e3c56414ea44057b760b
你çlongyongsen <longyongsen@wondersgroup.com>
2021-01-25 21:10:58 +0800

    关于前端

---
+++ boot-05-web-admin/src/test/java/com/atguigu/boot05webadmin/Boot05WebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot05webadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05WebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
